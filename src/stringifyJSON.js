45a986295e6a9d1a658e61391c9c0399861272ee
Judy Zaratan <mochicat8@gmail.com>
2014-09-22 23:45:31 -0700

    Implements stringifying on various data types and recursion on arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,27 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var stringify = "";
+  if (typeof obj === 'number' || typeof obj === 'boolean'){
+  	return stringify+=obj;
+  	}
+  if (obj === null){
+  	return 'null';
+  	}
+	if (Array.isArray(obj)=== true){
+		var arrStrStart = '[';
+		var arrStrEnd = ']';
+		var arrString ="";
+		for (var i = 0; i < obj.length; i++){
+			arrString+=stringifyJSON(obj[i]);
+			if (i < obj.length - 1){
+				arrString+=",";
+				}
+			}
+		return '['+arrString+']';
+		}
+if (typeof obj === 'string'){
+  	return "\""+ obj + "\"";
+  	}	
+			
 };
