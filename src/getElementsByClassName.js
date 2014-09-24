de1bc6ec606713c9878c079e6a504704b5942aeb
Judy Zaratan <mochicat8@gmail.com>
2014-09-23 21:15:10 -0700

    Implement function only with child nodes of body, does not implement to look children of child nodes and so on.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,35 @@
 //   return document.getElementsByClassName(className);
 // };
 
+/********** User notes
+//element.childNodes returns array of direct child and decendants, lists them 
+//in order of direct decendants
+
+//element.classList returns array of classes for that element specifially
+
+//document.body stores body of html 
+
+***********/
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var results = [];
+  var body = document.body;
+  if(body.className === className){
+  	results.push(body);
+  	}
+  
+console.log("This is the child node", body.childNodes);
+  for (var i = 0; i< body.childNodes.length; i++){
+  	console.log('this is the childnode.length', body.childNodes.length);
+	if(body.childNodes[i].nodeType === 1 && body.childNodes[i].className===className){
+		console.log('nodetype',body.childNodes[i].nodeType);
+		results.push(body.childNodes[i]);
+		}
+
+
+  }
+  console.log(results);
+  return results;
+
 };
