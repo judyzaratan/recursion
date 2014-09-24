05ca8f6d2440ad9ecec34225a806859ab8054f12
Judy Zaratan <mochicat8@gmail.com>
2014-09-24 01:02:56 -0700

    Implements getElementsByClass, massive cleanup of variables and functions needed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -15,23 +15,64 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
-  var results = [];
+  
+  //Stores results and returned variable
+  var results = [];		
+  
   var body = document.body;
+  
+  //Checks body for classname
   if(body.className === className){
   	results.push(body);
   	}
+  	
+  var checkMore;	
+  //DELETE later
+  //console.log("This is the child node", body.childNodes);
   
-console.log("This is the child node", body.childNodes);
-  for (var i = 0; i< body.childNodes.length; i++){
-  	console.log('this is the childnode.length', body.childNodes.length);
-	if(body.childNodes[i].nodeType === 1 && body.childNodes[i].className===className){
-		console.log('nodetype',body.childNodes[i].nodeType);
-		results.push(body.childNodes[i]);
+  function checkNodes(node){
+  //Add recursive function here
+	if(!node){
+		node = document.body;
 		}
+		
+  	for (var i = 0; i< node.childNodes.length; i++){
 
-
+  		//Checks if it is an element, proceed to do following
+		if(node.childNodes[i].nodeType === 1){
+		
+			//Checking 
+			if(node.childNodes[i].childNodes.length > 0)
+			{	
+				checkMore = node.childNodes[i];
+				console.log('Has more than 1 child node recursion expected', checkMore);
+				checkNodes(checkMore); //Recursion
+				
+			}
+		//Checks if there is more than one class
+			if(node.childNodes[i].classList.length > 1){
+		
+				var moreClass = node.childNodes[i].classList;
+			
+				for(var k=0; k < moreClass.length; k++){
+					//DELETE
+					console.log("this is the class name", moreClass[k]);
+				
+					if(moreClass[k] === className){
+						results.push(node.childNodes[i])
+					}
+				}
+			}	
+			
+		if (node.childNodes[i].className === className){
+			console.log('nodetype',node.childNodes[i].nodeType);
+			results.push(node.childNodes[i]);
+		}
+		}
+  	}
   }
-  console.log(results);
+  checkNodes();
+  console.log('This is the result of the function', results);
   return results;
 
 };
